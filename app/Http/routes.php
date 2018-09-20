143bcd9b5833c61950929af60ea1b7ec8b78ef3c
Fernnx2 <fernandoaguilera_@outlook.es>
2018-09-19 20:44:26 -0600

    se agregaron las rutas del modulo 1

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,3 +19,32 @@ Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
+
+//Rutas Ejercicio Modulo 1
+Route::get('index', function(){
+	echo "Pantalla Principal";
+});
+
+Route::get('login', function(){
+	echo "Login de Usuario";
+});
+
+Route::get('logaut', function(){
+	echo "Logaut de usuario";
+});
+
+Route::get('catalog', function(){
+	echo "Listado de Peliculas";
+});
+
+Route::get('catalog/show/{id}', function($id){
+	echo "Vista detalle con el id : " . $id;
+});
+
+Route::get('catalog/create', function(){
+	echo "Formulario para añadir pelicula";
+});
+
+Route::get('catalog/edit/{id}', function($id){
+	echo "Modificar Pelicula con id :" . $id;
+});
